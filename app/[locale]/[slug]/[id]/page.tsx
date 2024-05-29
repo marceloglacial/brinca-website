efd33e4431a297a1a431e370c5481908026d49be
marceloglacial <glacial@gmail.com>
2024-05-29 07:41:08 -0400

    fix menus

--- app/[locale]/[slug]/[id]/page.tsx
+++ app/[locale]/[slug]/[id]/page.tsx
@@ -3,16 +3,16 @@ import { Heading, Section } from '@marceloglacial/brinca-ui';
 
 export default async function Page({ params }: { params: PageParamsProps }) {
   const data = await getData('pages', params.id, params.locale);
-  const content = data.data[0];
+  const pageData = data.data[0];
 
-  if (!content) return <h1>Not found</h1>;
+  if (!pageData) return <h1>Not found</h1>;
 
   return (
     <Section>
       <Heading className='mb-4'>
-        <h1>{content.title}</h1>
+        <h1>{pageData.title}</h1>
       </Heading>
-      <Section dangerouslySetInnerHTML={{ __html: content.content }} />
+      <Section dangerouslySetInnerHTML={{ __html: pageData.content }} />
     </Section>
   );
 }
