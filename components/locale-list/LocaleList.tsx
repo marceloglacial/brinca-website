186d755b34994f95f9ceebea92aacbb5aff31814
marceloglacial <glacial@gmail.com>
2024-08-06 22:09:03 -0400

    initial language menu position

--- components/locale-list/LocaleList.tsx
+++ components/locale-list/LocaleList.tsx
@@ -1,16 +1,15 @@
 import { getDataByType } from '@/services';
 import { FC } from 'react';
 import { LocaleListItem } from './LocaleListItem';
+import { LocaleListItems } from './LocaleListItems';
 
 export const LocaleList: FC = async (): Promise<JSX.Element> => {
   const data = await getDataByType('locales');
   const locales: LocaleListType[] = data.data;
 
   return (
-    <div className='locale-list flex gap-4 justify-center items-center'>
-      {locales.map((item, index) => (
-        <LocaleListItem key={index} {...item} />
-      ))}
+    <div className='locale-list fixed top-4 -right-8 hover:right-0 transition-all grid grid-cols-2 gap-4 border rounded-full px-4 py-2 bg-white shadow-2xl rounded-tr-none rounded-br-none border-none'>
+      <LocaleListItems items={locales} />
     </div>
   );
 };
