96f69a89faf18b8bd49e42460b2800a84ac336fb
marceloglacial <glacial@gmail.com>
2024-11-06 00:28:42 -0500

    add cloudinary_folder

--- components/block/Block.tsx
+++ components/block/Block.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
-import { Embed, RichText } from '@/components';
+import { CloudinaryGallery, Embed, RichText } from '@/components';
 
 export const Block: FC<BlockProps> = (props): JSX.Element => {
   if (!props.content) return <></>;
 
+  console.log(props.content.type);
+
   const blockType: any = {
     youtube: <Embed url={props.content.value} type={props.content.type} />,
     content: <RichText content={props.content.value} />,
+    cloudinary_folder: <CloudinaryGallery path={props.content.value} />,
   };
 
   return <div className='block'>{blockType[props.content.type] || ''}</div>;
