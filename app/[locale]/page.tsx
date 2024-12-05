4b5099f729e206f5ee11912ec20be43b61fac32e
marceloglacial <glacial@gmail.com>
2024-12-04 19:55:20 -0500

    update vscode

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -10,6 +10,8 @@ export default async function Page(props: PageParamsType) {
 
   const content = result.data;
 
+  console.log(content);
+
   return (
     <Section spacing='xl'>
       <Content items={content.blocks} locale={params.locale} />
