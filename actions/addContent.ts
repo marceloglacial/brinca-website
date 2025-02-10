8ad694359fee0f43a3a6d95ccf6e57b0e2fd23e9
Marcelo <glacial@gmail.com>
2025-02-09 21:27:59 -0500

    add file upload

--- actions/addContent.ts
+++ actions/addContent.ts
@@ -1,14 +1,14 @@
 'use server';
-
 import { DICTIONARY } from '@/constants';
 import { addDoc, collection } from '@firebase/firestore';
-import { db } from '@/services/firebase';
+import { ref, uploadBytes, getDownloadURL } from '@firebase/storage';
+import { db, storage } from '@/services/firebase';
 
 export async function addContent(collectionId: string, formData: FormData, locale: LocalesType) {
   const data: { [key: string]: any } = {};
 
   for (const [key, value] of formData.entries()) {
-    const keysToRemove = ['formEndpoint', 'formType', 'formLocale', 'formTitle'].includes(key)
+    const keysToRemove = ['formEndpoint', 'formType', 'formLocale', 'formTitle',].includes(key)
 
     if (keysToRemove) continue;
 
@@ -16,7 +16,15 @@ export async function addContent(collectionId: string, formData: FormData, local
   }
 
   try {
+    const fileField = formData.get('logo');
+    if (fileField instanceof File) {
+      const fileName = `${Date.now()}_${fileField.name}`;
+      const storageRef = ref(storage, `partners/${fileName}`);
+      await uploadBytes(storageRef, fileField);
+      data.logo = await getDownloadURL(storageRef);
+    }
     await addDoc(collection(db, collectionId), data);
+
     return {
       status: 'success'
     }
