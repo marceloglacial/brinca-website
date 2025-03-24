6fc77e220199920d05cb14ec6a59ea3ad87edaa1
Marcelo <glacial@gmail.com>
2025-03-24 09:13:33 -0400

    fix nextjs eslint not working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')]
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    ignores: ['**/*.cy.*', '**/*.stories.*'],
+  },
+]
 
 export default eslintConfig
