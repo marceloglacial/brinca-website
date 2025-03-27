61b95041076e7847c8f7f28e8f9296e39c705fe1
DercilioFontes <dercilioafontes@gmail.com>
2025-03-26 22:44:26 -0400

    BRI-38 Add Social Icons on Footer

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,17 +1,28 @@
-import { LocaleList, Logo } from '@/components'
+import { Icon, LocaleList, Logo } from '@/components'
 import { NavBar as NavBarUi } from '@/components/ui'
 import Link from 'next/link'
 import { FC } from 'react'
 import { NavBarItems } from './NavBarItems'
 
 export const NavBar: FC<NavBarUiProps> = ({ variant = 'top', items }) => {
-  const componentStyles = variant === 'bottom' ? 'white' : undefined
+  const isBottom = variant === 'bottom';
+  const componentStyles = isBottom ? 'white' : undefined
   return (
     <NavBarUi variant={variant}>
       <NavBarUi.Brand>
         <Link href={'/'} aria-label='Home'>
           <Logo variant={componentStyles} />
         </Link>
+        {isBottom && (
+          <div className='pl-6 flex w-full justify-center gap-2 lg:justify-start'>
+            <a href='https://www.instagram.com/brinca_ottawa/' target='_blank' rel='noreferrer'>
+              <Icon variant='white' type='facebook' />
+            </a>
+            <a href='https://www.instagram.com/brinca_ottawa/' target='_blank' rel='noreferrer'>
+              <Icon variant='white' type='instagram' />
+            </a>
+          </div>
+        )}
       </NavBarUi.Brand>
       <NavBarItems variant={variant} items={items} localeList={<LocaleList />} />
     </NavBarUi>
--- types/icon.d.ts
+++ types/icon.d.ts
@@ -1,5 +1,7 @@
 type IconTypes = 'facebook' | 'instagram' | 'whatsapp' | 'linkedin' | 'loading'
+type IconVariants = 'white' | 'green'
 
 interface IconProps {
   type: IconTypes
+  variant?: IconVariants
 }
