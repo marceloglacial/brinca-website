4c6852ca23210443a3c100a2957a384c70db06e0
DercilioFontes <dercilioafontes@gmail.com>
2025-05-15 22:52:16 -0400

    Add API schemas and implement custom fetch logic

---
+++ lib/api.ts
@@ -0,0 +1,32 @@
+import { COLLECTIONS } from '@/constants'
+import { ApiResponseSchema, ParamsSchema } from '@/schemas/api'
+import { GetDataParams, NewApiResponse } from '@/types/new-api'
+
+const customFetch = async (baseUrl: string, params: GetDataParams): Promise<NewApiResponse> => {
+  try {
+    ParamsSchema.parse(params)
+
+    const searchParams = new URLSearchParams()
+    Object.entries(params).forEach(([key, value]) => {
+      if (value) searchParams.append(key, value.toString())
+    })
+    const url = `${baseUrl}${searchParams.toString() ? '?' + searchParams.toString() : ''}`
+    const response = await fetch(url, {
+      headers: new Headers({
+        'x-api-key': process.env.API_KEY!,
+        'Content-Type': 'application/json',
+      }),
+    })
+    const data = await response.json()
+
+    return ApiResponseSchema.parse(data)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    throw error
+  }
+}
+
+export const getAllPages = async () => {
+  const baseUrl = `${process.env.API_URL!}/${COLLECTIONS.PAGES}/`
+  return customFetch(baseUrl, {})
+}
