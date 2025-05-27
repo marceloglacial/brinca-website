335f2d48a9e397c00fe32ca369a4e2c1ea9f4ace
DercilioFontes <dercilioafontes@gmail.com>
2025-05-27 08:00:21 -0400

    Refactor CategoryListTitle to destructure locale from useParams for improved readability

--- components/partners-list/CategoryListTitle.tsx
+++ components/partners-list/CategoryListTitle.tsx
@@ -4,8 +4,7 @@ import { DICTIONARY } from '@/constants'
 import { useParams } from 'next/navigation'
 
 const CategoryListTitle: React.FC = () => {
-  const params = useParams()
-  const locale = params.locale as keyof typeof DICTIONARY.CATEGORIES
+  const { locale } = useParams<{ locale: LocalesType }>()
   const title = DICTIONARY.CATEGORIES[locale]
 
   return <h4 className='partners-list-categories__title'>{title}</h4>
