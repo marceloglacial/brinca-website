51a7604bc783dd30b6a7b49a4afa8f403c615f6f
Marcelo Freitas <glacial@gmail.com>
2026-03-25 00:15:34 +0000

    fix add buttons

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,16 @@ const __filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(__filename)
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        'localhost:3001',
+        'special-funicular-pjjg47gxpvwc6jqp-3000.app.github.dev',
+        '*.app.github.dev',
+      ],
+    },
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
